cc86dc6b14effd7def9e06949ab387bc63ed95c8
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-04-13 19:55:40 +0600

    Project Restarted

---
+++ src/Root/Root.jsx
@@ -0,0 +1,14 @@
+import { ToastContainer } from "react-toastify";
+
+const Root = () => {
+    return (
+        <>
+            <div>
+
+            </div>
+            <ToastContainer />
+        </>
+    );
+};
+
+export default Root;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Root/Root";
+
+const Router = createBrowserRouter([
+
+    {
+        path: "/",
+        Component: Root
+    },
+
+]);
+
+export default Router;
